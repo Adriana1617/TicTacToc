2204c9f1fff30e2eeed46f37488ed744f251b5b1
Adriana Enriquez <150649214+Adriana1617@users.noreply.github.com>
2026-02-13 23:16:11 -0600

    Remove duplicate SPDX license identifier

--- contracts/Achievement.sol
+++ contracts/Achievement.sol
@@ -1,4 +1,5 @@
 //SPDX-License-Identifier: GPL-3.0
+//SPDX-License-Identifier: GPL-3.0
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
